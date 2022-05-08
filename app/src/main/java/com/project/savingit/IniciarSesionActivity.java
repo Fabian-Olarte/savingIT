a651e77252dfbe6c85dedad4f037ecff69ffd261
andresito29 <70115938+AndresVR14@users.noreply.github.com>
2022-05-08 14:07:07 -0500

    IniciaSesion activity

---
+++ app/src/main/java/com/project/savingit/IniciarSesionActivity.java
@@ -0,0 +1,14 @@
+package com.project.savingit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IniciarSesionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion);
+    }
+}
