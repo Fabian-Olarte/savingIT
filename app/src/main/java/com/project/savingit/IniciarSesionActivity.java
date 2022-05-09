99a0bfb58418d8c5f1121ee770cf731975c7002a
Kenwuan4 <57266578+Kenwuan4@users.noreply.github.com>
2022-05-09 00:44:40 -0500

    funcion basica de firebase

--- app/src/main/java/com/project/savingit/IniciarSesionActivity.java
+++ app/src/main/java/com/project/savingit/IniciarSesionActivity.java
@@ -1,16 +1,32 @@
 package com.project.savingit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Pattern;
 
 public class IniciarSesionActivity extends AppCompatActivity {
 
     Button buttonCrearCuenta;
     Button buttonIniciarSesion;
+    EditText inputCorreo;
+    EditText inputContra;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,10 @@ public class IniciarSesionActivity extends AppCompatActivity {
 
         buttonCrearCuenta = findViewById(R.id.buttonCrearCuenta2);
         buttonIniciarSesion = findViewById(R.id.buttonIniciarSesion2);
+        inputCorreo = findViewById(R.id.inputCorreo);
+        inputContra = findViewById(R.id.inputContraseña);
+
+        auth = FirebaseAuth.getInstance();
 
         buttonCrearCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,9 +51,62 @@ public class IniciarSesionActivity extends AppCompatActivity {
         buttonIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(IniciarSesionActivity.this, MainActivity.class));
-                finish();
+
+                login();
+
             }
         });
     }
+
+    private void login(){
+        String email = inputCorreo.getText().toString().trim();
+        String contra = inputContra.getText().toString().trim();
+
+        if (email.isEmpty()){
+            inputCorreo.setError("Ingrese su correo");
+            inputCorreo.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            inputCorreo.setError("Por favor ingrese un correo valido");
+            inputCorreo.requestFocus();
+            return;
+        }
+
+        if (contra.isEmpty()){
+            inputContra.setError("Ingrese su contraseña");
+            inputContra.requestFocus();
+            return;
+        }
+
+        if (contra.length() < 6){
+            inputContra.setError("La contraseña debe tener minimo 6 caracteres");
+            inputContra.requestFocus();
+            return;
+        }
+
+        auth.signInWithEmailAndPassword(email,contra).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    startActivity(new Intent(IniciarSesionActivity.this, MainActivity.class));
+                    finish();
+                }
+
+                else {
+                    Toast.makeText(getApplicationContext(),"Ocurrio un error", Toast.LENGTH_LONG);
+                }
+            }
+        });
+
+    }
+
+    /*@Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        updateUI(currentUser);
+    }*/
 }
--- app/src/main/java/com/project/savingit/RegisterActivity.java
+++ app/src/main/java/com/project/savingit/RegisterActivity.java
@@ -1,14 +1,99 @@
 package com.project.savingit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText nombre, correo, contraseña;
+    Button crearCuenta;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        nombre = findViewById(R.id.inputNombreRegistro);
+        correo = findViewById(R.id.inputCorreoRegistro);
+        contraseña = findViewById(R.id.inputContraseñaRegistro);
+        crearCuenta = findViewById(R.id.buttonIniciarSesion2);
+
+        auth = FirebaseAuth.getInstance();
+
+        crearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nuevaCuenta();
+            }
+        });
+    }
+
+    private void nuevaCuenta(){
+
+        String nomb, email, pass;
+
+        nomb = nombre.getText().toString().trim();
+        email = correo.getText().toString().trim();
+        pass = contraseña.getText().toString().trim();
+
+        if (nomb.isEmpty()){
+            nombre.setError("Ingresa tu nombre");
+            nombre.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()){
+            correo.setError("Ingresa tu correo");
+            correo.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            correo.setError("Ingresa un correo valido");
+            correo.requestFocus();
+            return;
+        }
+
+        if (pass.isEmpty()){
+            contraseña.setError("Ingresa una contraseña");
+            contraseña.requestFocus();
+            return;
+        }
+
+        if (pass.length() <6){
+            contraseña.setError("La contraseña debe ser de 6 caracteres");
+            contraseña.requestFocus();
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                    finish();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Ocurrio un error", Toast.LENGTH_LONG);
+                }
+            }
+        });
     }
 }
